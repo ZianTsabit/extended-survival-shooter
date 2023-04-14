bd9bcc5c441b2a29e1168230d78a66499d94b4aa
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2023-04-14 13:59:00 +0700

    fix: enemy attack pet

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,14 +3,14 @@
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
+    Transform pet;
+    GameObject petObject;
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
     void Awake()
     {
-        //Cari game object with tag player
-   
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
         //Mendapatkan componen reference
@@ -22,10 +22,29 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        //Pindah ke player position
+        // Placed in update because the existence of Pet is not confirmed
+        petObject = GameObject.FindGameObjectWithTag("Pet");
+        if (petObject != null) pet = petObject.transform;
+        
+        
+        //Pindah ke player position
         if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0 && !ShopScript.afterQuestShopping)
         {
-            nav.SetDestination(player.position);
+            if (pet != null)
+            {
+                if (Vector3.Distance(transform.position, pet.position) < Vector3.Distance(transform.position, player.position))
+                {
+                    nav.SetDestination(pet.position);
+                }
+                else
+                {
+                    nav.SetDestination(player.position);
+                }
+            }
+            else
+            {
+                nav.SetDestination(player.position);
+            }
         }
         else //Stop moving
         {
