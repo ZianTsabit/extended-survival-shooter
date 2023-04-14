9ff4efb915384a3e67cb8cb9c7ddc07b8a4d3e72
Vionie Novencia Thanggestyo <13520006@std.stei.itb.ac.id>
2023-04-14 23:14:50 +0700

    fix: fix load bug

--- Assets/Scripts/Load/Load3Script.cs
+++ Assets/Scripts/Load/Load3Script.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Load3Script : MonoBehaviour
 {
     public TMPro.TMP_Text load3;
+    public Button button3;
     string filename = "save3.json";
 
     private void Start()
     {
+        if (!PlayerPrefs.HasKey("Save3"))
+        {
+            button3.enabled = false;
+        }
         load3.text = PlayerPrefs.GetString("Save3");
     }
 
@@ -21,11 +27,14 @@ public class Load3Script : MonoBehaviour
             TimeManager.prevSecond = savedItem.save_time;
             MoneyManager.prevMoney = savedItem.save_money;
             PlayerPrefs.SetString("PlayerName", savedItem.save_name);
+            ShopManager.isHavePet = savedItem.save_isHavePet;
+            ShopManager.isHaveAttacker = savedItem.save_isHaveAttacker;
+            ShopManager.isHaveBuffAura = savedItem.save_isHaveBuffAura;
+            ShopManager.isHaveHealer = savedItem.save_isHaveHealer;
+            PlayerBow.isEquipped = savedItem.save_bow;
+            PlayerShotgun.isEquipped = savedItem.save_shotgun;
+            PlayerMelee.isEquipped = savedItem.save_melee;
             SceneManager.LoadScene(savedItem.level_index);
-        }
-        else
-        {
-            SceneManager.LoadScene(1);
         }
 
     }
--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
@@ -8,13 +9,20 @@ public class MainMenu : MonoBehaviour
     {
         TimeManager.prevSecond = 0;
         MoneyManager.prevMoney = 0;
+        ShopManager.isHavePet = false;
+        ShopManager.isHaveAttacker = false;
+        ShopManager.isHaveBuffAura = false;
+        ShopManager.isHaveHealer = false;
+        PlayerBow.isEquipped = false;
+        PlayerShotgun.isEquipped = false;
+        PlayerMelee.isEquipped = false;
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit!");
-        Application.Quit();
+        EditorApplication.isPlaying = false;
     }
 }
 
