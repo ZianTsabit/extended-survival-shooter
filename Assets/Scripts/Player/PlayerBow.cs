61615c70f4ca864a31d0380ff99fc7dd7558dbf6
Kent <kent.delatinos@gmail.com>
2023-04-15 13:58:09 +0700

    feat: remove reload

--- Assets/Scripts/Player/PlayerBow.cs
+++ Assets/Scripts/Player/PlayerBow.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 public class PlayerBow : MonoBehaviour
 {
     [SerializeField]
-    private float reloadTime;
-
-    [SerializeField]
     private Arrow arrowPrefab;
 
     [SerializeField]
@@ -16,41 +13,12 @@ public class PlayerBow : MonoBehaviour
 
     public static bool isEquipped = false;
 
-    private bool isReloading;
-
-    void Awake()
-    {
-        currentArrow = Instantiate(arrowPrefab, spawnPoint);
-        currentArrow.transform.localPosition = Vector3.zero;
-        isReloading = false;
-    }
-
-    public void Reload()
-    {
-        if (isReloading || currentArrow != null) return;
-        isReloading = true;
-        StartCoroutine(ReloadAfterTime());
-    }
-
-    private IEnumerator ReloadAfterTime()
-    {
-        yield return new WaitForSeconds(reloadTime);
-        currentArrow = Instantiate(arrowPrefab, spawnPoint);
-        currentArrow.transform.localPosition = Vector3.zero;
-        isReloading = false;
-    }
-
     public void Fire(float firePower)
     {
-        if (isReloading || currentArrow == null) return;
+        currentArrow = Instantiate(arrowPrefab, spawnPoint);
+        currentArrow.transform.localPosition = Vector3.zero;
         var force = spawnPoint.TransformDirection(Vector3.up * firePower);
         currentArrow.Fly(force);
         currentArrow = null;
-        Reload();
-    }
-
-    public bool IsReady()
-    {
-        return (!isReloading && currentArrow != null);
     }
 }
